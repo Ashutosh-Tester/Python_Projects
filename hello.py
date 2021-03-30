18678946b334c028d6252d798375fcbaa9a84a70
Ashutosh-Tester <muskelon1111@gmail.com>
2021-03-30 11:15:00 +0530

    Committed first time

---
+++ hello.py
@@ -0,0 +1,7 @@
+# Python 3.9.2 (tags/v3.9.2:1a79785, Feb 19 2021, 13:30:23) [MSC v.1928 32 bit (Intel)] on win32
+# Type "help", "copyright", "credits" or "license()" for more information.
+print("Hello World")
+a=5
+print(a)
+a=a+5
+print(a)
