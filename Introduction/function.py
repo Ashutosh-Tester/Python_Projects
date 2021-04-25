dcba54be9f649aeb41e059671ea3120544b265b7
Ashutosh <ashutoshsingh.iimt@gmail.com>
2021-04-25 20:22:27 +0530

    Committed nth time

--- Introduction/function.py
+++ Introduction/function.py
@@ -1,3 +1,12 @@
 def sum(a,b):
      return a+b 
 print(sum(10,15))
+def product(a,b):
+    return a*b
+print(product(10,15))
+def division(a,b):
+    return a/b
+print(division(20,5))
+def subtraction(a,b):
+    return a-b
+print(subtraction(10,5))
