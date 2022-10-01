e758d16e16acc38674c568cd2e2bb8705389d5a0
AshutoshSingh47 <ashutoshsingh.iimt@gmail.com>
2022-10-01 23:29:53 +0530

    nn

--- Introduction/list2.py
+++ Introduction/list2.py
@@ -1,19 +1,4 @@
-"""list1=['hello','how','are','you']
-list2=['i','am','fine']
-print(list1)
-print(len(list1))
-for i in list1:
-    print(i,end=" ")
-print()
-for j in list2:
-    print(j,end=" ")
-list1.append("Neelay")
-list2.append("Neelesh")
-list1.insert(5,"xyz")
-print(list1)
-list1.extend(list2)
-print(list1)
-print(list1," ",list2)"""
+
 size=int(input("Enter the size of first array : "))
 A=[]
 print("Enter values")
