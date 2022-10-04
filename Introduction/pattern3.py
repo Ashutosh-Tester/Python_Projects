950dadc85941de7a7214c77a12ae350a0bf2f226
Ratnesh Kumar <ratneshrt19@gmail.com>
2022-10-04 20:14:11 +0530

    Added Python Patterns

---
+++ Introduction/pattern3.py
@@ -0,0 +1,11 @@
+def a(n):
+    num=1 
+    for i in range (0,n):
+        num=1
+      
+        for j in range (0,i+1):
+            print(num ,end="")
+            num=num+1
+        print("\r")
+n=5
+a(n)
---
+++ Introduction/pattern4.py
@@ -0,0 +1,17 @@
+def a(n):
+    num=1 
+    for i in range (0,n):
+        num=1
+        for j in range (0,i+1):
+            print(num ,end=" ")
+            num=num+1
+        print("\r")
+
+    for i in range(n+1,0,-1):
+        num=1
+        for j in range(0,i-1):
+            print(num,end=" ")
+            num=num+1
+        print("\r")
+n=5
+a(n)
